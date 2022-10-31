833901c6b73cb96663038dfe8d26347df61cfef3
Siphosenkosi Mthulisi Ndhlovu <sipho.ndhlovu@outlook.com>
2022-10-31 04:37:09 +0200

    Fixed padding big quote

--- frontend/components/sections/big-quote.js
+++ frontend/components/sections/big-quote.js
@@ -3,7 +3,7 @@ const BigQuote = ({ data }) => {
     return (
         <div className="bg-green-primary text-white">
             <div className="container my-16">
-                <div className="pt-16 w-48 font-bold text-xl after:content-[' '] after:block after:w-24 after:h-[5px] after:bg-yellow-400">
+                <div className="w-48 font-bold text-xl after:content-[' '] after:block after:w-24 after:h-[5px] after:bg-yellow-400">
                     {data.citation}
                 </div>
                 <div className="font-bold text-5xl text-center py-16 max-w-[30ch]">
